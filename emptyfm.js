cbc6a366cc7068675f0c6644991168ea728bf151
Nirmal Kumar R <tildezero@gmail.com>
2024-03-19 18:53:57 +0530

    New design and functionality

--- emptyfm.js
+++ emptyfm.js
@@ -1,9 +1,9 @@
 let player;
 
-document.addEventListener('DOMContentLoaded', function () {
-  player = videojs('emptyfm');
-  player.play();
-});
+// document.addEventListener('DOMContentLoaded', function () {
+// player = videojs('emptyfm');
+// player.play();
+// });
 
 async function fetchFMStations() {
   let countrySelect = document.getElementById("countryCode");
@@ -30,9 +30,19 @@ async function fetchFMStations() {
   
   const response = await fetch("https://de1.api.radio-browser.info/json/stations/search?" + queryParams);
   const stations = await response.json();
-  console.log(stations);
-  
-  changeSource(stations[1].url);
+
+  let fmList = document.getElementById("fmlist");
+
+  fmList.InnerHTML = "";
+  for (var k in stations) {
+    let atag = document.createElement("a");
+    atag.className = "fmitem";
+    atag.href = stations[k].url;
+    atag.innerText = stations[k].name;
+    atag.onclick = () => alert(atag.href);
+
+    fmList.appendChild(atag);
+  }
 }
 
 function changeSource(url) {
@@ -45,10 +55,10 @@ function listCountries() {
   fetch('./iso-3166-1-alpha2-countrycode.json')
     .then((response) => response.json())
     .then((json) => {
+      let selectElem = document.getElementById("countryCode");
+
       for (var k in json) {
         if (json.hasOwnProperty(k)) {
-	  let selectElem = document.getElementById("countryCode");
-	  
 	  let option = document.createElement("option");
 	  option.text = json[k];
           option.value = k;
@@ -63,10 +73,10 @@ function listLanguages() {
   fetch('./iso-639-lang.json')
     .then((response) => response.json())
     .then((json) => {
+      let selectElem = document.getElementById("languages");
+
       for (var k in json) {
         if (json.hasOwnProperty(k)) {
-	  let selectElem = document.getElementById("languages");
-	  
 	  let option = document.createElement("option");
 	  option.text = json[k];
           option.value = k;
