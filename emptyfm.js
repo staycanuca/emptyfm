27356c5db4baa5202d967e5de1ebf69444782317
Nirmal Kumar R <tildezero@gmail.com>
2024-05-19 19:58:52 +0530

    Enhance with Recents playlist

--- emptyfm.js
+++ emptyfm.js
@@ -1,112 +1,155 @@
 let player;
-
-// document.addEventListener('DOMContentLoaded', function () {
-// player = videojs('emptyfm');
-// player.play();
-// });
+let recents;
 
 async function fetchFMStations() {
-  let countrySelect = document.getElementById("countryCode");
-  let countryCode = countrySelect.value;
+    let countrySelect = document.getElementById("countryCode");
+    let countryCode = countrySelect.value;
 
-  let languagesSelect = document.getElementById("languages");
-  let langCode = languagesSelect.value;
+    let languagesSelect = document.getElementById("languages");
+    let langCode = languagesSelect.value;
 
-  let queryParams;
+    let queryParams;
 
-  if (!countryCode && !langCode) {
-    alert("You must choose either country or language, or both if you wish.");
-    return;
-  }
-  else if (!countryCode) {
-    queryParams = "language=" + langCode + "&hidebroken=true&order=clickcount&reverse=true"
-  }
-  else if (!langCode) {
-    queryParams = "countrycode=" + countryCode + "&hidebroken=true&order=clickcount&reverse=true"
-  }
-  else {
-    queryParams = "countrycode=" + countryCode + "&language=" + langCode + "&hidebroken=true&order=clickcount&reverse=true"
-  }
-
-  const response = await fetch("https://de1.api.radio-browser.info/json/stations/search?" + queryParams);
-  const stations = await response.json();
-
-  let fmList = document.getElementById("fmlist");
-
-  fmList.innerHTML = "";
-  for (var k in stations) {
-    let atag = document.createElement("a");
-    atag.className = "fmitem";
-    atag.href = stations[k].url;
-    atag.innerText = stations[k].name;
-    atag.onclick = (e) => {
-      e.preventDefault();
-      changeSource(atag.href);
+    if (!countryCode && !langCode) {
+        alert("You must choose either country or language, or both if you wish.");
+        return;
+    } else if (!countryCode) {
+        queryParams = "language=" + langCode + "&hidebroken=true&order=clickcount&reverse=true"
+    } else if (!langCode) {
+        queryParams = "countrycode=" + countryCode + "&hidebroken=true&order=clickcount&reverse=true"
+    } else {
+        queryParams = "countrycode=" + countryCode + "&language=" + langCode + "&hidebroken=true&order=clickcount&reverse=true"
     }
 
-    fmList.appendChild(atag);
-  }
+    const response = await fetch("https://de1.api.radio-browser.info/json/stations/search?" + queryParams);
+    const stations = await response.json();
+
+    let fmList = document.getElementById("fmlist");
+
+    fmList.innerHTML = "";
+    for (var k in stations) {
+        let atag = document.createElement("a");
+        atag.className = "fmitem";
+        atag.href = stations[k].url;
+        atag.innerText = stations[k].name;
+        atag.onclick = (e) => {
+            e.preventDefault();
+            let found = recents.find(({
+                name
+            }) => name === atag.innerText);
+            if (!found) {
+                addToRecents(recents, atag.innerText, atag.href);
+            }
+
+            changeSource(atag.href);
+        }
+
+        fmList.appendChild(atag);
+    }
+}
+
+function populateRecents() {
+	let saved = localStorage.getItem('emptyfm') ?? '{ "recents": [] }';
+    let savedObj = JSON.parse(saved);
+	recents = savedObj["recents"];
+	
+	for (recent of recents) {
+		constructRecentEl(recent.name, recent.url);	
+	}
+}
+
+function constructRecentEl(name, url) {
+	let recentsEl = document.getElementById("recentList");
+    let litag = document.createElement("li");
+    let atag = document.createElement("a");
+    atag.href = url;
+    atag.innerText = name;
+	atag.onclick = (e) => {
+    	e.preventDefault();
+		changeSource(atag.href);
+	};
+
+    litag.appendChild(atag);
+    recentsEl.appendChild(litag);
+}
+
+function addToRecents(savedObj, name, url) {
+    let recentObj = {
+        name: name,
+        url: url
+    };
+
+    recents.push(recentObj);
+
+    let recentsStr = JSON.stringify({ recents: recents });
+    localStorage.setItem('emptyfm', recentsStr);
+
+	constructRecentEl(name, url);
 }
 
 function changeSource(url) {
-  player = videojs('emptyfm');
-  mediaType = fetchMediaType(url);
-  player.src({ type: mediaType, src: url });
-  player.play();
+    player = videojs('emptyfm');
+    mediaType = fetchMediaType(url);
+    player.src({
+        type: mediaType,
+        src: url
+    });
+    player.play();
 }
 
 function fetchMediaType(url) {
-  last = url.split("/").pop();
-  switch (last) {
-    case "stream":
-      return "audio/mpeg";
-    default:
-      ext = last.split(".").pop();
-      switch (ext) {
-        case "m3u8":
-          return "application/x-mpegURL";
+    last = url.split("/").pop();
+    switch (last) {
+        case "stream":
+            return "audio/mpeg";
         default:
-          return "audio/mpeg";
-      }
-  }
+            ext = last.split(".").pop();
+            switch (ext) {
+                case "m3u8":
+                    return "application/x-mpegURL";
+                default:
+                    return "audio/mpeg";
+            }
+    }
 }
 
 function listCountries() {
-  fetch('./iso-3166-1-alpha2-countrycode.json')
-    .then((response) => response.json())
-    .then((json) => {
-      let selectElem = document.getElementById("countryCode");
+    fetch('./iso-3166-1-alpha2-countrycode.json')
+        .then((response) => response.json())
+        .then((json) => {
+            let selectElem = document.getElementById("countryCode");
 
-      for (var k in json) {
-        if (json.hasOwnProperty(k)) {
-          let option = document.createElement("option");
-          option.text = json[k];
-          option.value = k;
+            for (var k in json) {
+                if (json.hasOwnProperty(k)) {
+                    let option = document.createElement("option");
+                    option.text = json[k];
+                    option.value = k;
 
-          selectElem.appendChild(option);
-        }
-      }
-    })
+                    selectElem.appendChild(option);
+                }
+			}
+        })
 }
 
 function listLanguages() {
-  fetch('./iso-639-lang.json')
-    .then((response) => response.json())
-    .then((json) => {
-      let selectElem = document.getElementById("languages");
+    fetch('./iso-639-lang.json')
+        .then((response) => response.json())
+        .then((json) => {
+            let selectElem = document.getElementById("languages");
 
-      for (var k in json) {
-        if (json.hasOwnProperty(k)) {
-          let option = document.createElement("option");
-          option.text = json[k];
-          option.value = k;
+            for (var k in json) {
+                if (json.hasOwnProperty(k)) {
+                    let option = document.createElement("option");
+                    option.text = json[k];
+                    option.value = k;
 
-          selectElem.appendChild(option);
-        }
-      }
-    })
+                    selectElem.appendChild(option);
+                }
+            }
+        })
 }
 
+populateRecents();
 listCountries();
 listLanguages();
 
